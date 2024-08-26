1644e9cc265de4ab9e68121886808e0b72a66c55
Dipesh Ydv <dipeshy444@gmail.com>
2024-08-26 12:00:32 +0545

    adding Intent file

---
+++ app/src/main/java/com/example/intentactivity/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.intentactivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btn)
+
+        button.setOnClickListener {
+
+            intent = Intent(this, IntentActivity2::class.java)
+            startActivity(intent)
+
+        }
+
+
+    }
+}
